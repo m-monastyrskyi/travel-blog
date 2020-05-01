8f9a63a71a9cbdc20a418f52650ae83575240f58
Mykola Monastyrskyi <kolyan4ik@gmail.com>
2020-05-01 15:30:58 +0200

    create articles file

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,149 +1,20 @@
 import React from 'react';
+import ArticleShort from "./ArticleShort";
+import tmp from "../data/articles";
+console.log(tmp.articles[0].content);
 
 const Main = () => {
     return (
         <main className="main">
             <div className="container">
                 <div className="article__wrapper">
-                    <article className="article">
-                        <div className="article__thumb">
-                            <img src="https://placeimg.com/400/300/nature" alt="cute kitten"/>
-                        </div>
-                        <div className="article__info">
-                            <div className="article__meta">
-                                    <span className="article__category">
-                                      <p>World</p>
-                                    </span>
-                                <span className="article__date">
-                                        <p>April 20, 2020</p>
-                                    </span>
-                            </div>
-                            <div className="article__title">
-                                <h2>Hello in test article</h2>
-                            </div>
-                            <div className="article__subtitle">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, esse inventore
-                                    mollitia
-                                    quasi quos veniam!</p>
-                            </div>
-                        </div>
-                    </article>
-                    <article className="article">
-                        <div className="article__thumb">
-                            <img src="https://placeimg.com/400/301/people" alt="cute kitten"/>
-                        </div>
-                        <div className="article__info">
-                            <div className="article__meta">
-                                    <span className="article__category">
-                                      <p>World</p>
-                                    </span>
-                                <span className="article__date">
-                                        <p>April 20, 2020</p>
-                                    </span>
-                            </div>
-                            <div className="article__title">
-                                <h2>Hello in test article</h2>
-                            </div>
-                            <div className="article__subtitle">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, esse inventore
-                                    mollitia
-                                    quasi quos veniam!</p>
-                            </div>
-                        </div>
-                    </article>
-                    <article className="article">
-                        <div className="article__thumb">
-                            <img src="https://placeimg.com/400/302/nature" alt="cute kitten"/>
-                        </div>
-                        <div className="article__info">
-                            <div className="article__meta">
-                                    <span className="article__category">
-                                      <p>World</p>
-                                    </span>
-                                <span className="article__date">
-                                        <p>April 20, 2020</p>
-                                    </span>
-                            </div>
-                            <div className="article__title">
-                                <h2>Hello in test article</h2>
-                            </div>
-                            <div className="article__subtitle">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, esse inventore
-                                    mollitia
-                                    quasi quos veniam!</p>
-                            </div>
-                        </div>
-                    </article>
-                    <article className="article">
-                        <div className="article__thumb">
-                            <img src="https://placeimg.com/401/300/nature" alt="cute kitten"/>
-                        </div>
-                        <div className="article__info">
-                            <div className="article__meta">
-                                    <span className="article__category">
-                                      <p>World</p>
-                                    </span>
-                                <span className="article__date">
-                                        <p>April 20, 2020</p>
-                                    </span>
-                            </div>
-                            <div className="article__title">
-                                <h2>Hello in test article</h2>
-                            </div>
-                            <div className="article__subtitle">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, esse inventore
-                                    mollitia
-                                    quasi quos veniam!</p>
-                            </div>
-                        </div>
-                    </article>
-                    <article className="article">
-                        <div className="article__thumb">
-                            <img src="https://placeimg.com/402/300/people" alt="cute kitten"/>
-                        </div>
-                        <div className="article__info">
-                            <div className="article__meta">
-                                    <span className="article__category">
-                                      <p>World</p>
-                                    </span>
-                                <span className="article__date">
-                                        <p>April 20, 2020</p>
-                                    </span>
-                            </div>
-                            <div className="article__title">
-                                <h2>Hello in test article</h2>
-                            </div>
-                            <div className="article__subtitle">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, esse inventore
-                                    mollitia
-                                    quasi quos veniam!</p>
-                            </div>
-                        </div>
-                    </article>
-                    <article className="article">
-                        <div className="article__thumb">
-                            <img src="https://placeimg.com/401/301/people" alt="cute kitten"/>
-                        </div>
-                        <div className="article__info">
-                            <div className="article__meta">
-                                    <span className="article__category">
-                                      <p>World</p>
-                                    </span>
-                                <span className="article__date">
-                                        <p>April 20, 2020</p>
-                                    </span>
-                            </div>
-                            <div className="article__title">
-                                <h2>Hello in test article</h2>
-                            </div>
-                            <div className="article__subtitle">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, esse inventore
-                                    mollitia
-                                    quasi quos veniam!</p>
-                            </div>
-                        </div>
-                    </article>
-
+                    <ArticleShort/>
+                    <ArticleShort/>
+                    <ArticleShort/>
+                    <ArticleShort/>
+                    <ArticleShort/>
+                    <ArticleShort/>
+                    <ArticleShort/>
                 </div>
             </div>
         </main>
