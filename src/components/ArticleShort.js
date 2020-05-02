52d7092a2b0b3f029df7e2c55a7ef7bf8ad8172a
Mykola Monastyrskyi <kolyan4ik@gmail.com>
2020-05-02 17:20:33 +0200

    fetch data

--- src/components/ArticleShort.js
+++ src/components/ArticleShort.js
@@ -1,27 +1,25 @@
 import React from 'react';
 
-const ArticleShort = () => {
+const ArticleShort = ({title,subtitle,category,datePublished, imageThumb}) => {
     return (
         <article className="article">
             <div className="article__thumb">
-                <img src="https://placeimg.com/400/300/nature" alt="cute kitten"/>
+                <img src={imageThumb} alt="thumb image"/>
             </div>
             <div className="article__info">
                 <div className="article__meta">
                                     <span className="article__category">
-                                      <p>World</p>
+                                      <p>{category}</p>
                                     </span>
                     <span className="article__date">
-                                        <p>April 20, 2020</p>
+                                        <p>{datePublished}</p>
                                     </span>
                 </div>
                 <div className="article__title">
-                    <h2>Hello in test article</h2>
+                    <h2>{title}</h2>
                 </div>
                 <div className="article__subtitle">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, esse inventore
-                        mollitia
-                        quasi quos veniam!</p>
+                    <p>{subtitle}</p>
                 </div>
             </div>
         </article>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ArticleShort from "./ArticleShort";
-import tmp from "../data/articles";
-console.log(tmp.articles[0].content);
+//import tmp from "../data/articles";
+import {getPosts} from "../api/api";
 
 const Main = () => {
+//    const posts = getPosts();
+    const [posts, setPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        fetch("https://api.jsonbin.io/v3/b/5ea9904e07d49135ba47ba43", {
+            method: 'GET',
+            headers: {
+                'X-Master-Key': '$2b$10$fHqFEmM9UHUWnkXZ8JMGqeWItrVIWaPf.tI9/zIvYT9s1wy246UBu'
+            },
+        })
+            .then(resp => resp.json())
+            .then(resp => {
+                setPosts(resp.record.articles);
+                setIsLoading(false);
+            });
+    }, []);
+
     return (
         <main className="main">
             <div className="container">
                 <div className="article__wrapper">
-                    <ArticleShort/>
-                    <ArticleShort/>
-                    <ArticleShort/>
-                    <ArticleShort/>
-                    <ArticleShort/>
-                    <ArticleShort/>
-                    <ArticleShort/>
+                    {
+                        isLoading ? <h1>Loading</h1> : posts.map(el => <ArticleShort
+                            key={el.id}
+                        title={el.title}
+                            subtitle={el.subtitle}
+                            category={el.category}
+                            datePublished={el.datePublished}
+                            imageThumb={el.imageThumb}
+                        />)
+                    }
+
                 </div>
             </div>
         </main>
